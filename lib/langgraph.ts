84e4fbfe4565196235a52534d425bd7c2ce18867
sudatra <mail2sudatra@gmail.com>
2025-02-20 00:11:46 +0530

    created langGraph workflow and stateGraph with tool calls

--- lib/langgraph.ts
+++ lib/langgraph.ts
@@ -1,6 +1,19 @@
 import { ChatAnthropic } from '@langchain/anthropic';
 import { ToolNode } from '@langchain/langgraph/prebuilt';
 import wxflows from '@wxflows/sdk/langchain';
+import { START, END, MessagesAnnotation, StateGraph } from '@langchain/langgraph';
+import SYSTEM_MESSAGE from '@/constants/systemMessage';
+import { AIMessage, SystemMessage, trimMessages } from '@langchain/core/messages';
+import { ChatPromptTemplate, MessagesPlaceholder } from '@langchain/core/prompts';
+
+const trimmer = trimMessages({
+  maxTokens: 10,
+  strategy: 'last',
+  tokenCounter: (msgs) => msgs.length,
+  includeSystem: true,
+  allowPartial: false,
+  startOn: 'human'
+});
 
 const toolClient = new wxflows({
   endpoint: process.env.WXFLOWS_ENDPOINT || '',
@@ -37,6 +50,43 @@ const initializeAnthropicModel = () => {
   return model;
 }
 
+const shouldContinue = (state: typeof MessagesAnnotation.State) => {
+  const messages = state.messages;
+  const lastMessage = messages[messages.length - 1] as AIMessage;
+
+  if(lastMessage.tool_calls?.length) {
+    return 'tools';
+  }
+
+  if(lastMessage.content && lastMessage._getType() === 'tool') {
+    return 'agent';
+  }
+
+  return END;
+}
+
 const createWorkflow = () => {
-  
+  const model = initializeAnthropicModel();
+  const stateGraph = new StateGraph(MessagesAnnotation)
+  .addNode('agent', async (state) => {
+    const systemContent = SYSTEM_MESSAGE;
+    const promptTemplate = ChatPromptTemplate.fromMessages([
+      new SystemMessage(systemContent, {
+        cache_control: { type: 'ephemeral' }
+      }),
+      new MessagesPlaceholder('messages')
+    ]);
+
+    const trimmedMessages = await trimmer.invoke(state.messages);
+    const prompt = await promptTemplate.invoke({ messages: trimmedMessages });
+    const response = await model.invoke(prompt);
+
+    return { messages: [response] };
+  })
+  .addEdge(START, 'agent')
+  .addNode('tools', toolNode)
+  .addConditionalEdges('agent', shouldContinue)
+  .addEdge('tools', 'agent');
+
+  return stateGraph;
 }
